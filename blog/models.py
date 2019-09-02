ce77df829f75cee7076535222261195bc951a29e
xKINGERx <winnerlogin@gmail.com>
2019-09-02 22:47:04 +0500

    Version 1

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
